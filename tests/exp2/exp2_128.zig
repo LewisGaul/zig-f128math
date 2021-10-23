c9319e61123460097824855fb9a292104b50f54a
Lewis Gaul <lewis.gaul@gmail.com>
2021-10-23 13:30:13 +0100

    Get exact values for exp2_f128 tests - all passing

--- tests/exp2/exp2_128.zig
+++ tests/exp2/exp2_128.zig
@@ -28,19 +28,19 @@ const test_cases = [_]TestValue{
     Test( nan_f128,                nan_f128                ),
 
     // Sanity cases
-    Test(-0x1.02239f3c6a8f13dep+3, 0x1.e8d13c396f44f5p-9   ),
-    Test( 0x1.161868e18bc67782p+2, 0x1.4536746bb6f139f4p+4 ),
-    Test(-0x1.0c34b3e01e6e682cp+3, 0x1.890ca0c00b9a679cp-9 ),
-    Test(-0x1.a206f0a19dcc3948p+2, 0x1.622d4b0ebc6c2e5ap-7 ),
-    Test( 0x1.288bbb0d6a1e5bdap+3, 0x1.340ec7f3e607c5bep+9 ),
-    Test( 0x1.52efd0cd80496a5ap-1, 0x1.950eef4bc5450eeap+0 ),
-    Test(-0x1.a05cc754481d0bdp-2,  0x1.824056efc687c4f8p-1 ),
-    Test( 0x1.1f9ef934745cad6p-1,  0x1.79dfa14ab121da5p+0  ),
-    Test( 0x1.8c5db097f744257ep-1, 0x1.b5cead2247372396p+0 ),
-    Test(-0x1.5b86ea8118a0e2bcp-1, 0x1.3fd8ba33216b93cep-1 ),
+    Test(-0x1.02239f3c6a8f13dep+3, 0x1.e8d13c396f44f500bfc7cefe1304p-9 ),
+    Test( 0x1.161868e18bc67782p+2, 0x1.4536746bb6f139f3c05f40f3758dp+4 ),
+    Test(-0x1.0c34b3e01e6e682cp+3, 0x1.890ca0c00b9a679b66a1cc43e168p-9 ),
+    Test(-0x1.a206f0a19dcc3948p+2, 0x1.622d4b0ebc6c2e5980cda14724e4p-7 ),
+    Test( 0x1.288bbb0d6a1e5bdap+3, 0x1.340ec7f3e607c5bd584d33ade9aep+9 ),
+    Test( 0x1.52efd0cd80496a5ap-1, 0x1.950eef4bc5450eeabc992d9ba86ap+0 ),
+    Test(-0x1.a05cc754481d0bd0p-2, 0x1.824056efc687c4f8b3c7e1f4f9fbp-1 ),
+    Test( 0x1.1f9ef934745cad60p-1, 0x1.79dfa14ab121da4f38057c8f9f2ep+0 ),
+    Test( 0x1.8c5db097f744257ep-1, 0x1.b5cead22473723958363b617f84ep+0 ),
+    Test(-0x1.5b86ea8118a0e2bcp-1, 0x1.3fd8ba33216b93ceab3a5697c480p-1 ),
 
     // Some boundary cases specific to exp2
-    Test( 0x1p+14 - 0x1p-99,       0x1.ffffffffffffffffffffffffa746p+16383 ), // The last value before the exp gets infinite
+    Test( 0x1p+14 - 0x1p-99,       0x1.ffffffffffffffffffffffffd3a3p+16383 ), // The last value before the exp gets infinite
     Test( 0x1p+15,                 inf_f128                ), // The first value that gives infinite exp
     // Test(-0x1.74910d52d3051p+9,    0x1p-1074               ), // The last value before the exp flushes to zero
     // Test(-0x1.74910d52d3052p+9,    0x0p+0                  ), // The first value at which the exp flushes to zero
