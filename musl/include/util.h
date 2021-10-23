da1875b5f4dc95577913c9fdf9c8b7acc2868a0b
Lewis Gaul <lewis.gaul@gmail.com>
2021-10-23 13:29:26 +0100

    Switch exp2_128.c to use libquadmath for IEEE 754 standard of binary128

--- musl/include/util.h
+++ musl/include/util.h
@@ -2,14 +2,17 @@
 #define UTIL_H
 
 
-typedef long long unsigned uint128_t;
-typedef float              float32_t;
-typedef double             float64_t;
-typedef long double        float128_t;
+#include <quadmath.h>
+
+
+typedef __uint128_t  uint128_t;
+typedef float        float32_t;
+typedef double       float64_t;
+typedef __float128   float128_t;
 
 #define HEX32  "0x%08x"
 #define HEX64  "0x%016lx"
-#define HEX128 "0x%032llx"
+#define HEX128 "0x%016lx%016lx"
 
 #define DEBUG_ENABLED 1
 
@@ -17,6 +20,14 @@ typedef long double        float128_t;
     do {                            \
         if (DEBUG_ENABLED)          \
             printf(fmt,__VA_ARGS__);\
+    } while (0)
+
+// TODO: Not working for some reason... prints nothing with no warnings.
+#define quadmath_printf(fmt, ...)                               \
+    do {                                                        \
+        char buf[256] = {};                                     \
+        quadmath_snprintf(buf, sizeof(buf), fmt, __VA_ARGS__);  \
+        printf("%s", buf);                                      \
     } while (0)
 
 
