4ab5e288bc0d66d284380a0d91c4da586acdbe43
Lewis Gaul <lewis.gaul@gmail.com>
2021-11-14 17:44:26 +0000

    Add stderr printing in Zig main funcs

--- src/lib.zig
+++ src/lib.zig
@@ -221,17 +221,44 @@ pub fn singleInputFuncMain(comptime func: @TypeOf(exp)) !void {
     if (input_arg.len == 10) {
         const input_bits = try std.fmt.parseUnsigned(u32, input_arg, 0);
         const input = @bitCast(f32, input_bits);
+        std.debug.print(
+            "IN:  0x{X:0>8}  {[1]x}  {[1]e}\n",
+            .{ input_bits, input },
+        );
         const result = func(input);
+        const result_bits = @bitCast(u32, result);
+        std.debug.print(
+            "OUT: 0x{X:0>8}  {[1]x}  {[1]e}\n",
+            .{ result_bits, result },
+        );
         try stdout.print("0x{X:0>8}", .{@bitCast(u32, result)});
     } else if (input_arg.len == 18) {
         const input_bits = try std.fmt.parseUnsigned(u64, input_arg, 0);
         const input = @bitCast(f64, input_bits);
+        std.debug.print(
+            "IN:  0x{X:0>16}  {[1]x}  {[1]e}\n",
+            .{ input_bits, input },
+        );
         const result = func(input);
+        const result_bits = @bitCast(u64, result);
+        std.debug.print(
+            "OUT: 0x{X:0>16}  {[1]x}  {[1]e}\n",
+            .{ result_bits, result },
+        );
         try stdout.print("0x{X:0>16}", .{@bitCast(u64, result)});
     } else if (input_arg.len == 34) {
         const input_bits = try std.fmt.parseUnsigned(u128, input_arg, 0);
         const input = @bitCast(f128, input_bits);
+        std.debug.print(
+            "IN:  0x{X:0>32}  {[1]x}  {[1]e}\n",
+            .{ input_bits, input },
+        );
         const result = func(input);
+        const result_bits = @bitCast(u128, result);
+        std.debug.print(
+            "OUT: 0x{X:0>32}  {x}\n",
+            .{ result_bits, result },
+        );
         try stdout.print("0x{X:0>32}", .{@bitCast(u128, result)});
     } else {
         std.debug.print(
