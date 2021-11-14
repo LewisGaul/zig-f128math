ed83bbec5636fc3c5c8f87f11a08d3ec204387d1
Lewis Gaul <lewis.gaul@gmail.com>
2021-11-14 21:59:11 +0000

    Add empty log2 Zig test files

---
+++ tests/log2.zig
@@ -0,0 +1,5 @@
+comptime {
+    _ = @import("log2/log2_32.zig");
+    _ = @import("log2/log2_64.zig");
+    _ = @import("log2/log2_128.zig");
+}
--- tests/tests.zig
+++ tests/tests.zig
@@ -1,4 +1,5 @@
 comptime {
     _ = @import("exp.zig");
     _ = @import("exp2.zig");
+    _ = @import("log2.zig");
 }
